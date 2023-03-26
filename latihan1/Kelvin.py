97177575447492eda11aea73f0219db71adc2411
coniretno <coni.retno33@gmail.com>
2023-03-26 11:19:31 +0700

    Add files via upload

---
+++ latihan1/Kelvin.py
@@ -0,0 +1,11 @@
+print("Nama = Coni Retnosari")
+print("NIM = 221511017")
+print("Kelas = K2 Teknik Informatika")
+print("------------------------------")
+
+#Konversi Suhu
+    
+Celcius=float(input("Masukan Suhu = "))
+C=90
+K= C + 273
+print("Konvesi =",C,"dearajat Celcius adalah",K,"derajat Kelvin")
